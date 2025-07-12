72907d1743596007b50cd21b7c877ddf62524158
jwmock88 <jwmock@proton.me>
2025-07-12 22:33:06 +0100

    Updated footer and form

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,12 +6,12 @@ export default function Footer({ className = "footer container" }) {
   return (
     <footer className={className}>
       <div className="footer__content">
+        <h3 className="footer__title">~ Josh Mock ~</h3>
+        <div className="footer__line" aria-label="Visual divider"></div>
         <div className="footer__btns">
           <LinkedInBtn className={"footer__btn btn"} />
           <GitHubBtn className={"footer__btn btn"} />
         </div>
-        <hr className="footer__hr" aria-label="Visual divider" />
-        <h3 className="footer__title">~ Josh Mock ~</h3>
       </div>
     </footer>
   );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 export default function Layout() {
   return (
